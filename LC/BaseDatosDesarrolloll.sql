7f2bb545e0a2bfaf9ed7f9a44dd856e9d5e88a01
monicallano <monica.llano@correounivalle.edu.co>
2017-05-16 16:37:54 -0500

    Update BaseDatosDesarrolloll.sql

--- LC/BaseDatosDesarrolloll.sql
+++ LC/BaseDatosDesarrolloll.sql
@@ -16,12 +16,18 @@
 
 -- Creacion de tablas
 
+CREATE TYPE CARGO AS ENUM ('Mesero','Cajero','Gerente');
+CREATE TYPE CATEGORIA AS ENUM ('Res','Cerdo','Pollo','Pescado','Adicione','Postre','Bebida');
+CREATE TYPE TIPO_PEDIDO AS ENUM('Mesa','Llevar');
+CREATE TYPE ESTADO AS ENUM('Pagado','Sin Pagar');
+CREATE TYPE TIPO_PAGO AS ENUM('Debito','Credito', 'Efectivo');
+
 CREATE TABLE empleado
 (
 	id_empleado integer PRIMARY KEY,
 	nombres varchar(100) NOT NULL,
 	apellidos varchar(100) NOT NULL,
-	cargo varchar(20) NOT NULL,
+	cargo CARGO,
 	tel_fijo varchar(100) NOT NULL,
  	tel_cel varchar(100) NOT NULL,
 	Email varchar(100) NOT NULL,
@@ -36,7 +42,7 @@ CREATE TABLE turnos_semanales
 (
 	id_empleado integer NOT NULL,
 	turno varchar(100) NOT NULL,
- 	FOREIGN KEY (id_empleado) REFERENCES empleado 	(id_empleado),
+ 	FOREIGN KEY (id_empleado) REFERENCES empleado (id_empleado),
  	PRIMARY KEY (id_empleado, turno)
 );
 
@@ -46,9 +52,9 @@ CREATE TABLE item
 	id_item integer PRIMARY KEY,
 	descripcion varchar(100) NOT NULL,
 	nombre varchar(20) NOT NULL,
-	categoria varchar(100) NOT NULL,
+	categoria CATEGORIA NOT NULL,
  	precio integer NOT NULL,
-	foto varchar
+	foto varchar   
 );
 
 
@@ -57,9 +63,9 @@ CREATE TABLE pedido
 	id_pedido integer PRIMARY KEY,
 	hora_inicio varchar(100) NOT NULL,
 	hora_ultimo_item varchar(100),
-	tipo varchar(100) NOT NULL, /*puede ser pedido mesa o para 	llevar*/
+	tipo TIPO_PEDIDO NOT NULL, /*puede ser pedido mesa o para llevar*/
 	num_mesa integer,
-	id_empleado integer,
+	id_empleado integer NOT NULL,
 	FOREIGN KEY (id_empleado) REFERENCES empleado 	(id_empleado)
 );
 
@@ -76,46 +82,66 @@ CREATE TABLE item_pedido
 CREATE TABLE factura
 (
 	id_factura integer PRIMARY KEY,
-	estado varchar(100) NOT NULL,
-	forma_pago varchar(100) NOT NULL,
+	estado ESTADO NOT NULL,	
 	hora_pago varchar(100) NOT NULL,
-	valor_total integer NOT NULL, 
-	cedula_cliente integer NOT NULL,
+	valor_total integer NOT NULL, 	
 	id_pedido integer NOT NULL,
 	FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido)
+);
+
+CREATE TABLE items_de_factura
+(
+	nombre varchar(100) NOT NULL,
+	cantidad integer NOT NULL,
+	precio integer NOT NULL,
+	id_factura integer REFERENCES factura(id_factura),	
+	PRIMARY KEY (nombre , id_factura)
+);
+
+CREATE TABLE pagos
+(
+	id_pago integer NOT NULL,
+	tipo TIPO_PAGO NOT NULL,
+	valor integer NOT NULL,
+	cedula_cliente integer NOT NULL,
+	id_factura integer REFERENCES factura(id_factura),	
+	PRIMARY KEY (id_pago , id_factura)
 );
 
 
 
 -- Insercion de datos
 
-INSERT INTO empleado(id_empleado ,nombres ,apellidos ,cargo ,tel_fijo ,tel_cel ,Email ,direccion,tipo_documento ,contrase) VALUES(111,'Pepito','Perez','mesero','1234','4567','mesero@quehambre.com','cll5','cedula','111');
-INSERT INTO empleado(id_empleado ,nombres ,apellidos ,cargo ,tel_fijo ,tel_cel ,Email ,direccion,tipo_documento ,contrase) VALUES(222,'Armando','Casas','cajero','1234','4567','cajero@quehambre.com','cll6','cedula','222');
-INSERT INTO empleado(id_empleado ,nombres ,apellidos ,cargo ,tel_fijo ,tel_cel ,Email ,direccion,tipo_documento ,contrase) VALUES(333,'Pepa','Pig','gerente','1234','4567','gerenteo@quehambre.com','cll7','cedula','333');
-
+INSERT INTO empleado(id_empleado ,nombres ,apellidos ,cargo ,tel_fijo ,tel_cel ,Email ,direccion,tipo_documento ,contrase) VALUES(111,'Pepito','Perez','Mesero','1234','4567','mesero@quehambre.com','cll5','cedula','111');
+INSERT INTO empleado(id_empleado ,nombres ,apellidos ,cargo ,tel_fijo ,tel_cel ,Email ,direccion,tipo_documento ,contrase) VALUES(222,'Armando','Casas','Cajero','1234','4567','cajero@quehambre.com','cll6','cedula','222');
+INSERT INTO empleado(id_empleado ,nombres ,apellidos ,cargo ,tel_fijo ,tel_cel ,Email ,direccion,tipo_documento ,contrase) VALUES(333,'Pepa','Pig','Gerente','1234','4567','gerenteo@quehambre.com','cll7','cedula','333');
 
 INSERT INTO turnos_semanales (id_empleado ,turno) VALUES(111,'diurno-lunes');
 INSERT INTO turnos_semanales (id_empleado ,turno) VALUES(111,'nocturno-martes');
 INSERT INTO turnos_semanales (id_empleado ,turno) VALUES(222,'nocturno-jueves');
 INSERT INTO turnos_semanales (id_empleado ,turno) VALUES(333,'diurno-viernes');
 
+INSERT INTO item(id_item ,descripcion ,nombre ,categoria ,precio) VALUES(100,'rica-chuleta','chuleta-a-las-brasas','Res',25000);
+INSERT INTO item(id_item ,descripcion ,nombre ,categoria ,precio) VALUES(200,'rico-helado','helado-coco','Postre',15000);
+INSERT INTO item(id_item ,descripcion ,nombre ,categoria ,precio) VALUES(300,'rico-jugo','jugo-uva','Bebida',9000);
 
-INSERT INTO item(id_item ,descripcion ,nombre ,categoria ,precio) VALUES(100,'rica-chuleta','chuleta-a-las-brasas','plato-fuerte',25000);
-INSERT INTO item(id_item ,descripcion ,nombre ,categoria ,precio) VALUES(200,'rico-helado','helado-coco','postre',15000);
-INSERT INTO item(id_item ,descripcion ,nombre ,categoria ,precio) VALUES(300,'rico-jugo','jugo-uva','bebida',9000);
-
-
-INSERT INTO pedido(id_pedido ,hora_inicio ,hora_ultimo_item ,tipo ,num_mesa ,id_empleado) VALUES(1,'10:25','11:25','mesa','10',111);
-INSERT INTO pedido(id_pedido ,hora_inicio ,hora_ultimo_item ,tipo ,num_mesa ,id_empleado) VALUES(2,'16:25','17:25','llevar','6',111);
-INSERT INTO pedido(id_pedido ,hora_inicio ,tipo ,num_mesa ,id_empleado) VALUES(3,'14:25','mesa','11',111);
-INSERT INTO pedido(id_pedido ,hora_inicio ,tipo ,num_mesa ,id_empleado) VALUES(4,'14:25','llevar','11',222);
-
+INSERT INTO pedido(id_pedido ,hora_inicio ,hora_ultimo_item ,tipo ,num_mesa ,id_empleado) VALUES(1,'10:25','11:25','Mesa','10',111);
+INSERT INTO pedido(id_pedido ,hora_inicio ,hora_ultimo_item ,tipo ,num_mesa ,id_empleado) VALUES(2,'16:25','17:25','Llevar','6',111);
+INSERT INTO pedido(id_pedido ,hora_inicio ,tipo ,num_mesa ,id_empleado) VALUES(3,'14:25','Mesa','11',111);
+INSERT INTO pedido(id_pedido ,hora_inicio ,tipo ,num_mesa ,id_empleado) VALUES(4,'14:25','Llevar','11',222);
 
 INSERT INTO item_pedido(id_pedido ,id_item ,cantidad) VALUES(1,100,4);
 INSERT INTO item_pedido(id_pedido ,id_item ,cantidad) VALUES(1,300,4);
 INSERT INTO item_pedido(id_pedido ,id_item ,cantidad) VALUES(2,300,1);
 INSERT INTO item_pedido(id_pedido ,id_item ,cantidad) VALUES(3,200,2);
 
+INSERT INTO factura(id_factura ,estado ,hora_pago ,valor_total ,id_pedido) VALUES(1,'Sin Pagar','1050',25000,1);
+INSERT INTO factura(id_factura ,estado ,hora_pago ,valor_total ,id_pedido) VALUES(2,'Pagado','2350',9000,2);
 
-INSERT INTO factura(id_factura ,estado ,forma_pago ,hora_pago ,valor_total ,cedula_cliente ,id_pedido) VALUES(1,'pagado','efectivo','1050',25000,13255,1);
-INSERT INTO factura(id_factura ,estado ,forma_pago ,hora_pago ,valor_total ,cedula_cliente ,id_pedido) VALUES(2,'pagado','mixto','2350',9000,1758,2);
+INSERT INTO items_de_factura(nombre ,cantidad ,precio ,id_factura) VALUES('chuleton',1,20000,1);
+INSERT INTO items_de_factura(nombre ,cantidad ,precio ,id_factura) VALUES('juguito',1,5000,1);
+INSERT INTO items_de_factura(nombre ,cantidad ,precio ,id_factura) VALUES('jugo-uva',1,9000,2);
+
+INSERT INTO pagos(id_pago ,tipo ,valor ,cedula_cliente, id_factura) VALUES(1,'Debito',3000,1515,2);
+INSERT INTO pagos(id_pago ,tipo ,valor ,cedula_cliente, id_factura) VALUES(2,'Credito',3000,1656,2);
+INSERT INTO pagos(id_pago ,tipo ,valor ,cedula_cliente, id_factura) VALUES(3,'Efectivo',3000,5587,2);
